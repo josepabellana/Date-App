8cd06abc4a8de90b494e03610f719ce4e1295c00
Josep Abellana <josep.abe.pu@gmail.com>
2023-01-04 15:38:53 +0100

    setup

---
+++ dateapp/src/components/main/Main.tsx
@@ -0,0 +1,27 @@
+
+import './main.css'
+
+
+const Main = () => {
+
+    return(
+        <section className='main__container container grid'>
+        
+            <h1 className='section__title'>DATE</h1>
+            <div className="main__films">
+                <div className="main__film">
+
+                </div>
+                <div className="main__film">
+
+                </div>
+
+            </div>
+            <button className="main__discover">Show results</button>
+
+        </section>
+        )
+}
+
+
+export default Main;
