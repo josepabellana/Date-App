6c86864e40e750a5aea4710a097603aafa01221d
Josep Abellana <josep.abe.pu@gmail.com>
2023-01-23 23:58:12 +0100

    x

--- dateapp/src/components/main/Main.tsx
+++ dateapp/src/components/main/Main.tsx
@@ -32,11 +32,11 @@ const Main = ({ film }:{
         <div className="main__information">
             <h2>{film.title}</h2>
             <p>{film.release_date} · {film.genres.map((genre:any)=> <>{genre.name}, </>)} · {~~(film.runtime/60)}h {(film.runtime-~~(film.runtime/60)*60)%60}m</p>
-            {Object.keys(watchDetails).length > 0 ? 
+            {watchDetails.ES && watchDetails.ES.flatrate && Object.keys(watchDetails).length > 0 ? 
               <>
                 <h4>Where to Watch</h4>
                 <div className="main__toWatch">
-                  {watchDetails.Es.flatrate && watchDetails.ES.flatrate.map((info:any)=>{
+                  {watchDetails.ES.flatrate.map((info:any)=>{
                   return <img className="main__toWatch-logo" src={`https://image.tmdb.org/t/p/original${info.logo_path}`}></img>
                   })}
                 </div>
